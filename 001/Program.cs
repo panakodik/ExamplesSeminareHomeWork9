b5dba7f0ea26a982d5650277b748e8ebbc2ed2ff
Aleksandr <pandom@yandex.ru>
2023-02-17 00:46:49 +0300

    001 Натуральные от N до 1

---
+++ 001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от N до 1 включительно, N задано.
+
+void recursion(int n)
+{
+    if(n == 0) return;
+    Console.WriteLine(n);
+    recursion(n - 1);
+}
+
+recursion(10);
