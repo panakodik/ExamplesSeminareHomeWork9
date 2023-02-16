799aa715bdd712dbb9af69b5556b1dcc14b62a99
Aleksandr <pandom@yandex.ru>
2023-02-17 00:47:34 +0300

    002 Натуральные от M до N

---
+++ 002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от M до N, N и M заданы.
+
+int m = 5;
+int n = 10;
+
+void recursion(int n)
+{
+    if(n == m) return;
+    recursion(n - 1);
+    Console.WriteLine(n);
+}
+
+recursion(n);
