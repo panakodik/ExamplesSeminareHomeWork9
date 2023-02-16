787c29c42e10a4b2bffe0a158299f4cfc6d84767
Aleksandr <pandom@yandex.ru>
2023-02-17 00:48:10 +0300

    002 Сумма от M до N

---
+++ 003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int m = 3;
+int n = 6;
+int sum = 0;
+
+void recursion(int n)
+{
+    if(n == m) return;
+    Console.WriteLine(n);
+    sum = sum + n;
+    recursion(n - 1);
+}
+
+recursion(n);
+Console.WriteLine($"Сумма элементов равна {sum}");
+
